00d00f450a683bf3f68389dd0dbff1a1d554af42
Seth Moeckel <54327825+cosmotek@users.noreply.github.com>
2023-04-27 22:29:37 -0400

    moved ui html into separate file, updated styling by introducing tailwinds css

--- examples/simple_server.go
+++ examples/simple_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,35 +12,17 @@ func main() {
 	mockSender := fakemail.NewMockSender()
 	http.Handle("/sent_email/", mockSender.EmailViewer("/sent_email/"))
 
-	mockSender.Send(fakemail.MockEmail{
-		From:    "seth@test.com",
-		To:      []string{"bob@test.com"},
-		Subject: "Hello World",
-		Body:    "<h1>Hello World</h1>",
-		Metadata: map[string]string{
-			"test": "123",
-		},
-	})
-
-	mockSender.Send(fakemail.MockEmail{
-		From:    "seth@test.com",
-		To:      []string{"bob@test.com"},
-		Subject: "Hello World",
-		Body:    "<h1>Hello World</h1>",
-		Metadata: map[string]string{
-			"test": "123",
-		},
-	})
-
-	mockSender.Send(fakemail.MockEmail{
-		From:    "seth@test.com",
-		To:      []string{"bob@test.com"},
-		Subject: "Hello World",
-		Body:    "<h1>Hello World</h1>",
-		Metadata: map[string]string{
-			"test": "123",
-		},
-	})
+	for i := 0; i < 20; i++ {
+		mockSender.Send(fakemail.MockEmail{
+			From:    "seth@test.com",
+			To:      []string{"bob@test.com"},
+			Subject: "Hello World",
+			Body:    fmt.Sprintf("<h1>Hello World %d</h1>", i),
+			Metadata: map[string]string{
+				"test": "123",
+			},
+		})
+	}
 
 	log.Println("started email viewer at http://localhost:5050/sent_email")
 	http.ListenAndServe(":5050", nil)
